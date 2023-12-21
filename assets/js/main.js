7ae5e5736c3f9d9f9297b34aec743fbab7f4893b
ReisGiovanna <giovannareisdev@gmail.com>
2023-12-21 17:13:36 -0300

    Alterações para que os detalhes do Pokemon possam ser vistos ao ser clicados.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" id="pokemon-${pokemon.number}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -25,9 +25,35 @@ function convertPokemonToLi(pokemon) {
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML += newHtml;
+
+        pokemons.forEach((pokemon) => {
+            const pokemonElement = document.getElementById(`pokemon-${pokemon.number}`);
+            pokemonElement.addEventListener('click', () => {
+                openPokemonDetailsPage(pokemon);
+            });
+        });
+    });
+}
+
+function openPokemonDetailsPage(pokemon) {
+    const newPage = window.open('', '_blank');
+    newPage.document.write(`
+        <html>
+        <head>
+            <title>${pokemon.name} Details</title>
+            <link rel="stylesheet" href="/assets/css/details.css">
+        </head>
+        <body>
+            <h1>${pokemon.name} details:</h1>
+            <p class="detail">Number: #${pokemon.number}</p>
+            <p class="detail">Type: ${pokemon.type}</p>
+            <p class="detail">Types: ${pokemon.types.join(', ')}</p>
+            <img class="imgDetail" src="${pokemon.photo}" alt="${pokemon.name}">
+        </body>
+        </html>
+    `);
 }
 
 loadPokemonItens(offset, limit)
